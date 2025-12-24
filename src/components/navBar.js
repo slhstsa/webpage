82c0858a7eee634710273385da35b78b372256be
Not-Victor <vitorjonesanicio@gmail.com>
2025-12-24 09:11:51 -0600

    Made navbar sticky and animated it as you scroll

---
+++ src/components/navBar.js
@@ -0,0 +1,22 @@
+import './navBar.css';
+
+function Navigation()
+{
+  return(
+    <div class="center-header">
+      <div class="banner-links-header">
+        <a href="#" class="header-link">
+          Map
+        </a>
+        <a href="#" class="header-link">
+          Resources
+        </a>
+        <a href="#" class="header-link">
+          Community
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default Navigation;
