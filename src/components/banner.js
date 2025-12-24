9dc87545335995f19efd5785297babfe54163c61
Not-Victor <vitorjonesanicio@gmail.com>
2025-12-24 11:36:07 -0600

    Playing around with like liquid glass looking stuff

--- src/components/banner.js
+++ src/components/banner.js
@@ -4,6 +4,9 @@ import Search from './search'
 function Banner(){
   return(
     <div class="banner-container">
+      <div class="banner-title">
+        We Live Together:   <p>Katy Texas Resources</p>
+      </div>
       <img src="images/katy-stock.jpg" alt="katy/houston Skyline" class="banner-image"/>
       <div  class="search-bar-banner">
         <Search/>
