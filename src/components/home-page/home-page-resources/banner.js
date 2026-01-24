b886f30fd6d031d93bdf22e0c196be7ebe273b90
Not-Victor <vitorjonesanicio@gmail.com>
2026-01-24 13:26:07 -0600

    sc

--- src/components/home-page/home-page-resources/banner.js
+++ src/components/home-page/home-page-resources/banner.js
@@ -2,11 +2,11 @@ import "./banner.css"
 
 function Banner(){
   return(
-    <div class="banner-container">
-      <div class="banner-title">
+    <div className="banner-container">
+      <div className="banner-title">
         We Live Together:   <p>Katy Texas Resources</p>
       </div>
-      <img src="/images/katy-stock.jpg" alt="katy/houston Skyline" class="banner-image"/>
+      <img src="/images/katy-stock.jpg" alt="katy/houston Skyline" className="banner-image"/>
     </div>
   );
 }
