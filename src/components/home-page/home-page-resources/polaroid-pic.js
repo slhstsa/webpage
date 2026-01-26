99ca6f809ec23aaa1218ca08c8814d472d0fcc2c
Arya_Salem09 <88246378+EpicGamerGlobal@users.noreply.github.com>
2026-01-25 19:36:27 -0600

    styled resources and events

--- src/components/home-page/home-page-resources/polaroid-pic.js
+++ src/components/home-page/home-page-resources/polaroid-pic.js
@@ -1,13 +1,23 @@
-import './polaroid.css'
+import "./polaroid.css";
+import { Link } from "react-router-dom";
 
-function Polaroid({imageurl, text}){
-  return(
-
+function Polaroid({ imageurl, text, to }) {
+  const content = (
     <div className="polaroid-container">
-      <img className="photo" src={imageurl} alt="placeholder-text"/>
-      <div className="polaroid-text"> {text}</div>
+      <img className="photo" src={imageurl} alt={text} />
+      <div className="polaroid-text">{text}</div>
     </div>
   );
+
+  if (to) {
+    return (
+      <Link className="polaroid-link" to={to} aria-label={text}>
+        {content}
+      </Link>
+    );
+  }
+
+  return content;
 }
  
 export default Polaroid;
